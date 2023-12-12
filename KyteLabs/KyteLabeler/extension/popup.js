2b8915b212cd4f72217088b1bfa1ac7aa0fb50aa
ivfarias <imivanfarias@gmail.com>
2023-12-12 16:08:19 -0300

    Changes to be committed: 	modified:   KyteLabs/KyteLabeler/extension/popup.html 	modified:   KyteLabs/KyteLabeler/extension/popup.js 	modified:   KyteLabs/KyteLabeler/index.mjs

--- KyteLabs/KyteLabeler/extension/popup.js
+++ KyteLabs/KyteLabeler/extension/popup.js
@@ -1,26 +1,36 @@
-document.getElementById('generateButton').addEventListener('click', function () {
-    var xmlUrl = document.getElementById('xmlUrl').value;
-    var labelSize = document.getElementById('labelSize').value;
+document.addEventListener('DOMContentLoaded', function () {
+    var generateButton = document.getElementById('generateButton');
+    if (generateButton) {
+        generateButton.addEventListener('click', function () {
+            var xmlUrl = document.getElementById('xmlUrl').value;
+            var labelSize = document.getElementById('labelSize').value;
 
-    fetch('http://localhost:3000/generate-barcodes', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ xmlUrl: xmlUrl, size: labelSize })
-    })
-        .then(response => response.blob())
-        .then(blob => {
-            const url = window.URL.createObjectURL(blob);
-            chrome.downloads.download({
-                url: url,
-                filename: 'barcodes.pdf' // Optional: specify a filename for the download
-            });
-        })
-        .catch(error => {
-            console.log(error); // Log the error for debugging
-            // Handle error
+            fetch('http://localhost:3000/generate-barcodes', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ xmlUrl: xmlUrl, size: labelSize })
+            })
+                .then(response => response.blob())
+                .then(blob => {
+                    const url = window.URL.createObjectURL(blob);
+                    chrome.downloads.download({
+                        url: url,
+                        filename: 'barcodes.pdf' // Optional: specify a filename for the download
+                    });
+                })
+                .catch(error => {
+                    console.log(error); // Log the error for debugging
+                    // Handle error
+                });
         });
+    }
+
+    var catalogButton = document.querySelector('.catalog-btn');
+    if (catalogButton) {
+        catalogButton.addEventListener('click', toggleSections);
+    }
 });
 
 function toggleSections() {
