b38ab63e6df4dde20cb7beaaa5692eded7198f63
ivfarias <imivanfarias@gmail.com>
2024-08-09 15:20:53 -0300

    modified:   scripts/multi-step-form.js 	modified:   templates/forms/multi-step-form.html

--- scripts/multi-step-form.js
+++ scripts/multi-step-form.js
@@ -1,11 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const form = document.getElementById('LeadForm');
+/**
+ * Manage small forms, validating them and applying a "steps" feature
+ */
 
-	function getAttributionInfo() {
+const initLeadForm = function (id = 'LeadForm', validationRules = {}) {
+	var alias = `leadScript_${id}`;
+
+	// Function to set visibility of elements
+	var setVisible = function (elm, isVisible) {
+		elm.setAttribute('class', `${elm.getAttribute('class').replace('hidden', '')} ${isVisible ? 'show' : 'hidden'}`);
+	};
+
+	// Function to validate form fields
+	var validate = function (step) {
+		var elm = step ? window[alias].form.getElementsByClassName(`step${step}`)[0] : window[alias].form;
+		var fields = elm.getElementsByTagName('*');
+		for (var f of fields) {
+			var name = f.getAttribute('name');
+			if (
+				(f.hasAttribute('required') && ((f.type === 'checkbox' && !f.checked) || !!!f.value)) ||
+				(validationRules[name] && !validationRules[name](f.value))
+			) return false;
+		}
+		return true;
+	};
+
+	// Function to show a specific step in the form
+	var showStep = function (step) {
+		if (step > window[alias].steps || step < 1) return;
+		window[alias].currStep = step;
+		for (var s = 1; s <= window[alias].steps; s++)
+			setVisible(window[alias].form.getElementsByClassName(`step${s}`)[0], (step === s));
+		setVisible(window[alias].prevBtn, (step > 1));
+		setVisible(window[alias].nextBtn, (step < window[alias].steps));
+		setVisible(window[alias].submitBtn, (step === window[alias].steps));
+	};
+
+	// Function to capture attribution data from URL or local storage
+	var captureAttributionData = function () {
 		const params = new URLSearchParams(window.location.search);
 		const attributionFields = [
 			'utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term',
-			'cid', 'fbclid', 'gclid', 'ttclid', 'ref', 'referrer', 'campaignid',
+			'cid', 'fbclid', 'gclid', 'ttclid', 'referrer', 'campaignid',
 			'adgroupid', 'keyword', 'gadid', 'fbadid', 'ttadid'
 		];
 
@@ -14,112 +49,44 @@ document.addEventListener('DOMContentLoaded', function () {
 			const value = params.get(field) || localStorage.getItem(field);
 			if (value) {
 				attributionInfo[field] = value;
-				form.elements[field].value = value;
+				if (window[alias].form.elements[`mauticform[${field}]`]) {
+					window[alias].form.elements[`mauticform[${field}]`].value = value;  // Populate hidden field
+				}
 			}
 		});
+
 		return attributionInfo;
-	}
-
-	const validate = (step) => {
-		const elm = step ? form.querySelector(`.step${step}`) : form;
-		const fields = elm.querySelectorAll('[name]');
-		const validationRules = {
-			email: (value) => {
-				const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-1]\d{2}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
-				return re.test(String(value));
-			}
-		};
-
-		for (let f of fields) {
-			const name = f.getAttribute('name');
-			if (f.hasAttribute('required') && ((f.type === 'checkbox' && !f.checked) || !f.value)) {
-				console.log(`Validation failed: required field '${name}' is missing or empty.`);
-				return false;
-			}
-			if (validationRules[name] && !validationRules[name](f.value)) {
-				console.log(`Validation failed: field '${name}' with value '${f.value}' did not pass custom validation.`);
-				return false;
-			}
-		}
-		return true;
 	};
 
-	const showStep = (step) => {
-		if (step > form.steps || step < 1) return;
-		form.currStep = step;
-		for (let s = 1; s <= form.steps; s++) {
-			setVisible(form.querySelector(`.step${s}`), step === s);
-		}
-		setVisible(form.prevBtn, step > 1);
-		setVisible(form.nextBtn, step < form.steps);
-		setVisible(form.submitBtn, step === form.steps);
+	// Initialize the form
+	window[alias] = {
+		form: document.getElementById(id),
+		steps: Number(document.getElementById(id).getAttribute('data-steps')),
+		submitBtn: document.getElementById(id).getElementsByClassName('btn-submit')[0]
 	};
 
-	const setVisible = (elm, isVisible) => {
-		elm.classList.toggle('hidden', !isVisible);
-		elm.classList.toggle('show', isVisible);
-	};
+	// Capture and populate attribution data
+	const attributionData = captureAttributionData();
 
-	form.addEventListener('submit', function (e) {
-		e.preventDefault();
+	// Handle form submission
+	window[alias].submitBtn.addEventListener('click', function (evt) {
+		evt.preventDefault();
+		if (!validate()) return false;
 
-		const mauticPayload = {
-			email: form.elements['email'].value,
-			monthly_sales: form.elements['monthly_sales'].value,
-			opt_in: form.elements['opt-in'].checked,
-			formTitle: form.elements['formTitle'].value,
-			...getAttributionInfo()
-		};
+		const redirectUrl = window[alias].form.elements['mauticform[return]'].value;
+		const queryParams = new URLSearchParams(attributionData).toString();
+		window.location.href = `${redirectUrl}&${queryParams}`;
 
-		fetch('https://marketing.kyte.is/api/contacts/new', {
-			method: 'POST',
-			headers: {
-				'Authorization': 'Basic YWRtaW5BcGk6IVBvN1RCbTAzOTRdaHh5SU5udzkzSg==',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(mauticPayload)
-		}).then(response => response.json())
-			.then(data => {
-				console.log('Success:', data);
-				const attributionInfo = getAttributionInfo();
-				const queryParams = new URLSearchParams(attributionInfo).toString();
-				return window.location.href = `https://pos.auth.kyteapp.com?${queryParams}`;
-			})
-			.catch(error => {
-				console.error('Error:', error);
-			});
+		window[alias].form.submit();
+		window[alias].submitBtn.disabled = true;
 	});
 
-	const initLeadForm = function (id = 'LeadForm') {
-		const alias = `leadScript_${id}`;
-		form.steps = Number(form.getAttribute('data-steps'));
-		form.submitBtn = form.querySelector('.btn-submit');
-
-		form.submitBtn.addEventListener('click', (evt) => {
-			evt.preventDefault();
-			if (!validate()) {
-				console.log('Validation failed');
-				return false;
-			}
-			console.log('Validation passed, submitting form');
-			form.submit();
-			form.submitBtn.disabled = true;
-		});
-
-		if (form.steps > 0) {
-			form.prevBtn = form.querySelector('.btn-prev');
-			form.nextBtn = form.querySelector('.btn-next');
-			form.prevBtn.addEventListener('click', (evt) => {
-				evt.preventDefault();
-				showStep(form.currStep - 1);
-			});
-			form.nextBtn.addEventListener('click', (evt) => {
-				evt.preventDefault();
-				if (validate(form.currStep)) showStep(form.currStep + 1);
-			});
-			showStep(1);
-		}
-	};
-
-	initLeadForm('LeadForm');
-});
+	// Handle multi-step form
+	if (window[alias].steps > 0) {
+		window[alias].prevBtn = window[alias].form.getElementsByClassName('btn-prev')[0];
+		window[alias].nextBtn = window[alias].form.getElementsByClassName('btn-next')[0];
+		window[alias].prevBtn.addEventListener('click', function (evt) { evt.preventDefault(); showStep(window[alias].currStep - 1); });
+		window[alias].nextBtn.addEventListener('click', function (evt) { evt.preventDefault(); if (validate(window[alias].currStep)) showStep(window[alias].currStep + 1); });
+		showStep(1);
+	}
+};
